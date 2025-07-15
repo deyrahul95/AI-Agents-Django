7ac3daa5fc531ea47c140e3fd31cdef5cd26f7ff
Rahul Dey <rahuldey9623@gmail.com>
2025-07-15 15:04:23 +0000

    Added memory to the agent

--- src/ai/agents.py
+++ src/ai/agents.py
@@ -4,7 +4,7 @@ from ai.llms import get_openai_model
 from ai.tools import get_document, list_documents
 
 
-def get_document_agent():
+def get_document_agent(checkpointer=None):
     model = get_openai_model()
     document_tools = [list_documents, get_document]
 
@@ -12,5 +12,6 @@ def get_document_agent():
         model=model,
         tools=document_tools,
         prompt="You are a helpful assistant in managing a user's documents within this app",
+        checkpointer=checkpointer,
     )
     return agent
